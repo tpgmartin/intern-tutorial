07a9277312f19d675cd99668c9dd4e5ce6430c7d
Tom Martin <tom@tpgmartin.com>
2017-03-14 18:19:19 +0000

    Complete tutorial

---
+++ tests/functional/index.js
@@ -0,0 +1,28 @@
+define(function (require) {
+  var registerSuite = require('intern!object');
+  var assert = require('intern/chai!assert');
+
+  registerSuite({
+    name: 'index',
+
+    'greeting form': function () {
+      return this.remote
+				.get(require.toUrl('index.html'))
+				.setFindTimeout(5000)
+				.findByCssSelector('body.loaded')
+				.findById('nameField')
+					.click()
+					.type('Elaine')
+					.end()
+				.findByCssSelector('#loginForm input[type=submit]')
+					.click()
+					.end()
+				.findById('greeting')
+				.getVisibleText()
+				.then(function (text) {
+					assert.strictEqual(text, 'Hello, Elaine!',
+						'Greeting should be displayed when the form is submitted');
+				});
+    }
+  });
+});
